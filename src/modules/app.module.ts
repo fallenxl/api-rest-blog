db8de0bad8d1722825236726478a87a186d78377
Fallen <axlsntz.dev@gmail.com>
2023-09-07 18:13:16 -0600

    added auth module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     UserModule,
@@ -14,6 +15,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
+    AuthModule,
   ],
   controllers: [],
   providers: [],
